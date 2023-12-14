fd21196ee2461c97eb84ce58dc2ee7883a8875de
Gamze Karali <gnurkarali@gmail.com>
2023-12-14 16:38:14 +0300

    ders 2

---
+++ ProductProject/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductProject.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserList() 
+        { 
+            return View();
+        
+        }
+        public ActionResult NewUser()
+        {
+            return View();
+
+        }
+        public ActionResult ToDoList()
+        {
+            return View();
+
+        }
+    }
+}
